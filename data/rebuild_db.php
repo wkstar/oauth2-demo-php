12983ff571b41f2eb3f99f4535619b342b74a9e0
Tom Mead <tom.mead@iteesystems.com>
2015-04-17 12:22:01 +0100

    sql into rebuild

--- data/rebuild_db.php
+++ data/rebuild_db.php
@@ -7,10 +7,12 @@ $db = $storage->getDb();
 // rebuild the DB
 //$db = $storage;
 //$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+/*
 $db->exec('CREATE TABLE oauth_clients (client_id TEXT, client_secret TEXT, redirect_uri TEXT)');
 $db->exec('CREATE TABLE oauth_access_tokens (access_token TEXT, client_id TEXT, user_id TEXT, expires TIMESTAMP, scope TEXT)');
 $db->exec('CREATE TABLE oauth_authorization_codes (authorization_code TEXT, client_id TEXT, user_id TEXT, redirect_uri TEXT, expires TIMESTAMP, scope TEXT)');
 $db->exec('CREATE TABLE oauth_refresh_tokens (refresh_token TEXT, client_id TEXT, user_id TEXT, expires TIMESTAMP, scope TEXT)');
+*/
 //$db->exec('CREATE TABLE oauth_users (username TEXT, password TEXT, first_name TEXT, last_name TEXT)');
 
 // add test data
